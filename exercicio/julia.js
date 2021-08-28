f1109e44825302cab459b305e6dd6e08704ee313
juliarebecca <88255877+juliarebecca@users.noreply.github.com>
2021-08-28 00:52:11 -0300

    adicionando meu repositorio

---
+++ exercicio/julia.js
@@ -0,0 +1,22 @@
+// EXERCICIOS COM JAVASCRIPT:
+
+//USE ALERT PARA MOSTRAR NA TELA UM "SEJA BEM VINDO AO MEU SITE"
+var ayalaFilha = "É a coisa mais importante";
+alert(ayalaFilha);
+
+//USER ALERT MENSAGEM SIMPLES
+alert("Te amo filha");
+
+//USE ALERT PARA MOSTRAR O RESULTADO DA SOMA ENTRE 87 + 96
+alert (87+96);
+
+//USE ALERT PARA MOSTRAR O RESULADO DA MULTIPLICACAO ENTRE 5 E 8
+alert(5*8);
+
+//CRIE UMA LISTA DE COMPRAS
+var lista = ["kiwi", "Maçã", "Uva", "Banana"];
+alert(lista);
+
+//CREI UM ALERT PARA MOSTRAR IDADE
+var idade = 34
+alert (idade)
