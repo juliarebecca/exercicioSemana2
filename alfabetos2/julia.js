049c5de6407b091ce2f2f6514a24b42e4e6263cd
juliarebecca <88255877+juliarebecca@users.noreply.github.com>
2021-09-13 19:33:30 -0300

    Delete alfabetos2 directory

--- alfabetos2/julia.js
+++
@@ -1,8 +0,0 @@
-var letra = prompt("DIGITE UMA LETRA:");
-var resposta = letra.toUpperCase();
-switch (resposta == 'A' || resposta ==  'E' || resposta == 'I' || resposta == 'O' || resposta == 'U'){
-    case true: alert('Você digitou uma vogal!');
-        break;
-    default: alert("Você digitou uma consoante!");
-        break;
-}
